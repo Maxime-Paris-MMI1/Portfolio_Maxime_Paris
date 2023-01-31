120230a5b8da682c4ca58bb4493850c2a56b2fac
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-01-31 16:17:08 +0100

    mise en forme sans le svg

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,12 +1,14 @@
 <template>
   <header>
     <div class="flex justify-between">
-      <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[100px] object-contain" alt="logo blanc de mon portfolio">
-      <ul class="text-white text-right font-unbounded text-xl">
-        <li class="mb-[50px]">Mes projets</li>
-        <li class="mb-[50px]">Contact</li>
-        <li>À propos</li>
-      </ul>
+      <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[50px] object-contain" alt="logo blanc de mon portfolio">
+      <div class="mt-[90px]">
+        <ul class="text-white text-right font-unbounded text-xl">
+          <li class="mb-[50px] mr-[11px]">Mes projets</li>
+          <li class="mb-[50px] mr-[11px]">Contact</li>
+          <li class="mr-[11px]">À propos</li>
+        </ul>
+      </div>
     </div>
   </header>
 </template>
