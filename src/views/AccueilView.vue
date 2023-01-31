36dcd315fe0d1cc9b48f87013c88a7940aed5495
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-01-31 16:07:40 +0100

    commencement header et import typo

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,4 +1,14 @@
 <template>
+  <header>
+    <div class="flex justify-between">
+      <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[100px]" alt="logo blanc de mon portfolio">
+      <ul class="text-white text-right font-unbounded">
+        <li>Mes projets</li>
+        <li>Contact</li>
+        <li>À propos</li>
+      </ul>
+    </div>
+  </header>
 </template>
 
 <script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,27 @@ module.exports = {
         // Ici vos couleurs exportées de Figma
       },
       "fontFamily": {
+        "ubuntu": "Ubuntu",
+        "unbounded": "Unbounded",
         // Ici vos familles de polices exportées de Figma
         // Ne pas oublier de les charger dans '/index.html' !
       },
+      
+        "fontSize": {
+         "2xs": "0.625rem",
+         "xs": "0.75rem",
+         "sm": "0.875rem",
+         "base": "1.125rem",
+         "lg": "1.25rem",
+         "xl": "1.5625rem"
+        },
+        "borderRadius": {
+         "none": "0",
+         "xs": "0.3125rem",
+         "sm": "2.0625rem",
+         "default": "2.375rem"
+        }
+       
     },
 
   },
