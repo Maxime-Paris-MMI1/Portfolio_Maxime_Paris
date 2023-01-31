ac1cb86edbf1e12daad629c9eeaecc2e3c06e3dd
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-01-31 16:13:21 +0100

    mise en forme menu

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,10 +1,10 @@
 <template>
   <header>
     <div class="flex justify-between">
-      <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[100px]" alt="logo blanc de mon portfolio">
-      <ul class="text-white text-right font-unbounded">
-        <li>Mes projets</li>
-        <li>Contact</li>
+      <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[100px] object-contain" alt="logo blanc de mon portfolio">
+      <ul class="text-white text-right font-unbounded text-xl">
+        <li class="mb-[50px]">Mes projets</li>
+        <li class="mb-[50px]">Contact</li>
         <li>À propos</li>
       </ul>
     </div>
