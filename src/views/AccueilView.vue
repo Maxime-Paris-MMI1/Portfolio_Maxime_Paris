9b3c006bfa61f572bdd3ca682e01937a4fad2707
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-01 13:32:36 +0100

    test avec logo

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,7 +1,5 @@
 <template>
   <header>
-    <div class='cursor' id="cursor">
-    </div>
     <div class="flex justify-between">
       <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[50px] object-contain" alt="logo blanc de mon portfolio">
       <div class="mt-[90px]">
@@ -15,34 +13,6 @@
   </header>
   <h1 class="text-white mt-80">test pour curseur</h1>
   <img src="../../public/images/photo_perso.png" class="absolute z-20" alt="">
-  
 </template>
 
-<style>
 
-.cursor {
-  position: fixed;
-  border-radius: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  pointer-events: none;
-  left: -100px;
-  top: 50%;
-  mix-blend-mode: normal;
-  background-color: white;
-  z-index: 10000;
-  border: 10px solid white;
-  height: 60px;
-  width: 60px;
-  transition: all 40ms ease-out;
-}
-</style>
-
-<script>
-
-  var cursor = document.getElementById("cursor");
-  document.body.addEventListener("mousemove", function(e) {
-      cursor.style.left = e.clientX + "px",
-      cursor.style.top = e.clientY + "px";
-  });
-
-</script>
