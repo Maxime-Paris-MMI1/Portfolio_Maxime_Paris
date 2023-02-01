197e4f1e614221aa0ffcde6547d3759a9b702899
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-01 11:00:31 +0100

    changement de curseur fonctionnel pour les images

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
+
 <template>
   <RouterView />
+  <div class="cursor p-20">
+    <h1 class="text-white text-center">TEST</h1>
+  </div>
+  
 </template>
 
-<script>
-</script>
+<style>
+  .cursor {
+    cursor: url('../public/images/logo_blanc.webp')  47 47, auto;
+  }
+</style>
+
