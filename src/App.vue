a40738d0a29005d339ab828fa54e11b187d7ec2a
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-01 13:20:30 +0100

    foctionnel que dans le localhost

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 
 <template>
   <RouterView />
-  <div class='cursor' id="cursor">
-  </div>
+
+
 
 </template>
 
 
-<style>
-.cursor {
-  position: fixed;
-  border-radius: 50%;
-  transform: translateX(-50%) translateY(-50%);
-  pointer-events: none;
-  left: -100px;
-  top: 50%;
-  mix-blend-mode: normal;
-  background-color: white;
-  z-index: 10000;
-  border: 10px solid white;
-  height: 60px;
-  width: 60px;
-  transition: all 40ms ease-out;
-}
-</style>
-
-<script>
-
-  var cursor = document.getElementById("cursor");
-  document.body.addEventListener("mousemove", function(e) {
-      cursor.style.left = e.clientX + "px",
-      cursor.style.top = e.clientY + "px";
-  });
-
-</script>
 
