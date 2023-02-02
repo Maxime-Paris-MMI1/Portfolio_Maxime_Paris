6c428f4b05aeb12ecc97d1d4476fad228ee1b730
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-02 20:25:20 +0100

    mise en place de la font bold

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-between items-start">
       <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[90px] object-contain" alt="logo blanc de mon portfolio">
       <div class="mt-[90px] relative z-30">
-        <ul class="text-white text-right font-unbounded text-xl">
+        <ul class="text-white text-right font-unbounded text-xl font-bold">
 
           <div class="flex justify-end">
             <li class="mb-[50px] mr-[11px]">Mes projets</li>
