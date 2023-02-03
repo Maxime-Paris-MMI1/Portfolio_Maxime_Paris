cd875c1baa0e1e0b8b6d9b734bfb07bc97208134
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-03 09:00:31 +0100

    avancement front jusqu'à projet

---
+++ src/components/Card_projet.vue
@@ -0,0 +1,15 @@
+<div class="w-[365px] h-[365px] relative">
+    <img
+      src="rectangle-13.png"
+      class="w-[365px] h-[365px] absolute left-[-1px] top-[-1px] object-cover"
+    />
+    <div
+      class="w-[365px] h-[111px] absolute left-[-1px] top-[253px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0"
+    ></div>
+    <div class="w-[229px] h-[50px] absolute left-[67px] top-[294px] bg-black"></div>
+    <p
+      class="w-[365px] h-[186px] absolute left-0 top-[303px] text-[25px] font-bold text-center text-white"
+    >
+      E-morphoz
+    </p>
+  </div>
--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -2,21 +2,21 @@
   <header>
     <div class="flex justify-between items-start">
       <img src="../../public/images/logo_blanc.webp" class=" ml-[100px] mt-[90px] object-contain" alt="logo blanc de mon portfolio">
-      <div class="mt-[90px] relative z-30">
+      <div class="mt-[90px] relative z-30 lg:flex hidden">
         <ul class="text-white text-right font-unbounded text-xl font-bold">
 
           <div class="flex justify-end">
-            <li class="mb-[50px] mr-[11px]">Mes projets</li>
+            <li class="mb-[50px] mr-[11px] animation_texte">Mes projets</li>
             <Barre_menu class="mt-[15px]"/>
           </div>
 
           <div class="flex justify-end">
-            <li class="mb-[50px] mr-[11px]">Contact</li>
+            <li class="mb-[50px] mr-[11px] animation_texte">Contact</li>
             <Barre_menu class="mt-[15px]"/>
           </div>
 
           <div class="flex justify-end">
-            <li class="mr-[11px]">À propos</li>
+            <li class="mr-[11px] animation_texte">À propos</li>
             <Barre_menu class="mt-[15px]"/>
           </div>
 
@@ -51,9 +51,44 @@
       <span class="txt t2">Artiste 3D - Motion Design - 3D VFX - Webdesign - Création de logo - Affiche -&nbsp;</span>
     </div>
   </section>
+  <section class="mt-[163px] ml-[100px]">
+    <img src="../../public/images/mes-projets-stroke.webp" alt="mes projets sans fond">
+    <h2 class="font-unbounded text-[50px] text-white font-bold -ml-[3px] -mt-[34px]">MES PROJETS</h2>
+    <H2 class="font-unbounded text-[50px] text-white font-bold -ml-[3px] -mt-[40px]">MES PROJETS</H2>
+
+    <div class=" flex flex-row justify-around mt-[50px]">
+      <button class="text-white font Unbounded text-xl font-bold border px-5 py-2 hover:bg-white hover:text-black">Tout</button>
+      <button class="text-white font Unbounded text-xl font-bold border px-5 py-2 hover:bg-white hover:text-black">Éducatif</button>
+      <button class="text-white font Unbounded text-xl font-bold border px-5 py-2 hover:bg-white hover:text-black">Personnel</button>
+    </div>
+
+  </section>
+  <section>
+    <div class="border-t-4 border-white mt-[47px]"></div>
+
+    <div class="mx-auto w-3/4 justify-center items-center gap-7 mt-20 mb-20 grid grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
+
+    </div>
+
+  </section>
 </template>
 
 <style>
+
+.animation_texte{
+  box-shadow: inset 0 0 0 0 #fff;
+  color: #fff;
+  margin: 0 -.25rem;
+  padding: 0 .25rem;
+  transition: color .3s ease-in-out, box-shadow .3s ease-in-out;
+}
+
+.animation_texte:hover {
+  box-shadow: inset 250px 0 0 0 #fff;
+  color: black;
+}
+
+
 .container2{
   display: flex;
   overflow: hidden;
