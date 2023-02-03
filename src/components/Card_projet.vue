2a2b2fd36298b0826cbbd22bd98ff7a1b16e1650
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-03 13:37:52 +0100

    chgangement curseur + essai animation

--- src/components/Card_projet.vue
+++ src/components/Card_projet.vue
@@ -5,10 +5,13 @@
           :src="img_presentation"
           class="w-[365px] h-[365px] absolute left-[-1px] top-[-1px] object-cover"
         />
-        <div
-          class="w-[365px] h-[111px] absolute left-[-1px] top-[253px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0"
-        ></div>
+        <!-- <div class="w-[365px] h-[111px] absolute left-[-1px] top-[253px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0">
+        </div> -->
+        <div class="w-[365px] h-[111px] hover:h-[450px] mt-[250px] hover:mt-0 absolute left-[-1px] top-[0px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0">
+        </div>
+
         <div class="w-[229px] h-[50px] absolute left-[67px] top-[294px] bg-black"></div>
+        
         <p
           class="w-[365px] h-[70px] absolute left-0 top-[303px] text-[25px] font-bold text-center text-white"
         >
