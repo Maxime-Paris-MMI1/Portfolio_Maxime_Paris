81fd68bbb9e3382db9074529ae59ceec73e5c407
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-02-22 13:44:59 +0100

    amélioration drastique des hover sur les card

--- src/components/Card_projet.vue
+++ src/components/Card_projet.vue
@@ -1,26 +1,58 @@
 <template>
     <RouterLink class="curseur" :to="lien">
       <div class="w-[365px] h-[365px] relative">
-        <img
-          :src="img_presentation"
-          class="w-[365px] h-[365px] absolute left-[-1px] top-[-1px] object-cover"
-        />
-        <div class="w-[365px] h-[111px] hover:h-[450px] mt-[250px] hover:mt-0 absolute left-[-1px] top-[0px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0">
+        <!--  -->
+        <div class="w-[365px] h-[365px] absolute left-[-1px] top-[-1px]">
+          <div class="image-zoom">
+            <img
+              :src="img_presentation"
+              class="image"
+            />
+          </div>
+        </div>
+        <div class="w-[350px] h-[111px] mt-[250px]  absolute left-[-1px] top-[0px] bg-gradient-to-b from-[#9c9c9c]/20 to-white/0">
 
         </div>
 
-        <div class="w-[229px] h-[50px] absolute left-[67px] top-[294px] bg-black"></div>
-        
-        <p
-          class="w-[365px] h-[70px] absolute left-0 top-[303px] text-[25px] font-bold text-center text-white"
-        >
-          {{titre}}
-        </p>
+        <div>
+                  <!-- <div class="w-[229px] h-[50px] absolute left-[60px] top-[280px] bg-black cadre"></div>  -->
+        </div>
+        <div class="flex justify-center w-[365px]">
+          <p
+            class="absolute top-[280px] text-[25px] font-bold text-center text-white bg-black px-5 py-2"
+          >
+            {{titre}}
+          </p>
+        </div>
       </div>
         
     </RouterLink>
 
 </template>
+
+<style>
+
+.cadre:hover{
+  height: 365px; ;
+
+}
+.image-zoom{
+  width: 350px;
+  overflow: hidden;
+}
+
+.image{
+  width: 100%;
+  transition: transform .2s ease-in-out;
+}
+
+.image:hover{
+  -ms-transform: scale(1.3); /* IE 9 */
+  -webkit-transform: scale(1.3); /* Safari 3-8 */
+  transform: scale(1.3);
+}
+
+</style>
 
 <script>
 
