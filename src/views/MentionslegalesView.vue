f4af49623f34e99e231d51985481db2088929fb4
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-03-05 14:43:57 +0100

    mentions légales responsive

--- src/views/MentionslegalesView.vue
+++ src/views/MentionslegalesView.vue
@@ -15,10 +15,67 @@
         <div class="border-t-4 border-white"></div>
     </section>
   </div>
+  <section>
+    <div class="mb-10 text-white mt-10">
+        <h3 class="font-unbounded font-[700] text-white sm:mx-10 xxxs:mx-3
+              lg:text-[35px]
+              md:text-[35px]
+              sm:text-[30px]
+              xs:text-[25px]
+              xxs:text-[20px]
+              xxxs:text-[16px]">Contact</h3>
+    </div>
 
-        <div class="mb-40 text-white">
-            <h3 class="text-white">Information de contact</h3>
-        </div>
+    <div class="mb-10 sm:mx-10 xxxs:mx-3 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px]">
+      <p>Editeur du site : Maxime Paris</p>
+      <p>Adresse : 1 avenue de la principauté de Montbéliard 25250 Montbéliard</p>
+      <p>Numéro de téléphone : +33(0)6 36 47 01 96</p>
+      <div class="flex justify-start gap-3">
+        <p>Adresse email :</p>
+        <a href="mailto:maximepro.contact25@gmail.com" class="underline">maximepro.contact25@gmail.com</a>
+      </div>
+      <p>Hébergeur du site : Infomaniak</p>
+      <p>Adresse de l'hébergeur : Rue Eugène Marziano 25 1227 Les Acacias (GE)</p>
+    </div>
+
+    <div class="mb-10 text-white mt-10">
+        <h3 class="font-unbounded font-[700] text-white sm:mx-10 xxxs:mx-3
+              lg:text-[35px]
+              md:text-[35px]
+              sm:text-[30px]
+              xs:text-[25px]
+              xxs:text-[20px]
+              xxxs:text-[16px]">Information</h3>
+    </div>
+
+    <div class="mb-10 sm:mx-10 xxxs:mx-3 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px]">
+      <p>Le site a été réalisé dans un cadre éducatif au sein du MMI de Montbéliard.</p>
+      <p>Il ne dispose d'aucun système de paiement ni de formulaire de quelque nature que ce soit.</p>
+      <p>Le site et l'ensemble de son contenu (textes, images, vidéos, etc.) sont la propriété exclusive de Maxime Paris.</p>
+      <p>Le contenu du site est protégé par les lois françaises et internationales relatives à la propriété intellectuelle.</p>
+    </div>
+
+    <div class="mb-10 text-white mt-10">
+        <h3 class="font-unbounded font-[700] text-white sm:mx-10 xxxs:mx-3
+              lg:text-[35px]
+              md:text-[35px]
+              sm:text-[30px]
+              xs:text-[25px]
+              xxs:text-[20px]
+              xxxs:text-[16px]">Droit</h3>
+    </div>
+
+    <div class="mb-10 sm:mr-96 sm:ml-10 xxxs:ml-3 xxxs:mx-auto text-white font-ubuntu xxxs:text-[14px] sm:text-[16px]">
+      <p class="mb-2">Toute reproduction, représentation, diffusion ou exploitation de quelque nature que ce soit, même partielle, du contenu du site est strictement interdite sans l'autorisation préalable de Maxime Paris.</p>
+      <p class="mb-2">Maxime Paris ne saurait être tenu responsable des dommages directs ou indirects qui pourraient résulter de l'accès ou de l'utilisation du site, y compris l'inaccessibilité, les pertes de données, détériorations, destructions ou virus qui pourraient affecter l'équipement informatique de l'utilisateur.</p>
+      <p class="mb-2">Les liens hypertextes présents sur le site peuvent renvoyer vers des sites tiers. Maxime Paris décline toute responsabilité quant au contenu de ces sites.</p>
+      <p class="mb-2">Le site utilise des cookies pour améliorer l'expérience utilisateur. Les utilisateurs peuvent désactiver les cookies dans les paramètres de leur navigateur. Les données collectées par les cookies sont utilisées exclusivement par Maxime Paris et ne sont pas transmises à des tiers.</p>
+      <p>Les présentes mentions légales sont régies par le droit français. Tout litige relatif à l'utilisation du site sera soumis à la compétence exclusive des tribunaux français.</p>
+    </div>
+
+
+  </section>
+
 
     <footer>
         <Footer_total/>
