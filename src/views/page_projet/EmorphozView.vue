99fed122bd7c9c0bdcbc0ab37410cc171aa370e5
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2023-03-07 11:35:50 +0100

    responsive élément page projets

--- src/views/page_projet/EmorphozView.vue
+++ src/views/page_projet/EmorphozView.vue
@@ -20,29 +20,43 @@
 
 
     <div class="column">
-        <div class="row">
-            
-            <div class="heading-content text-black">
-                <h4 class="bg-white text-center font-unbounded text-[25px]">Présentation</h4>
-                <div class="text-white text-lg">
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim nihil delectus sed asperiores id harum iste. Quidem assumenda, optio iusto commodi dolor, magni deserunt corporis expedita corrupti, molestiae ducimus exercitationem!
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odio facilis corrupti possimus similique, iste eaque, natus doloribus, eius consequuntur dolor tempore pariatur eveniet neque? Perspiciatis officia doloribus omnis optio.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe in iusto eius vero quae, laudantium fuga eaque ipsum. Quia nostrum accusantium omnis, alias fuga illo ratione porro. Repudiandae, maiores
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere ipsa aperiam praesentium temporibus dicta aspernatur architecto! Optio similique autem error deserunt fuga commodi repellat quisquam accusamus laboriosam, voluptatibus quidem sit!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, qui? Dolorem recusandae animi debitis, saepe optio mollitia repudiandae tempore laudantium officia aliquam ipsam minima eaque vitae molestiae ipsum dolor harum.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium est laboriosam commodi sed libero eveniet quam obcaecati totam illo? Rerum magnam esse, aliquid autem explicabo aspernatur maiores ullam quod quis.
-                    </p>
+
+
+            <div class="row">
+                
+                <div class="heading-content text-black">
+                    <h4 class="bg-white text-center font-unbounded text-[25px]">Présentation</h4>
+    
+                    <div class="text-white text-lg hidden sm:flex">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim nihil delectus sed asperiores id harum iste. Quidem assumenda, optio iusto commodi dolor, magni deserunt corporis expedita corrupti, molestiae ducimus exercitationem!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odio facilis corrupti possimus similique, iste eaque, natus doloribus, eius consequuntur dolor tempore pariatur eveniet neque? Perspiciatis officia doloribus omnis optio.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe in iusto eius vero quae, laudantium fuga eaque ipsum. Quia nostrum accusantium omnis, alias fuga illo ratione porro. Repudiandae, maiores
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere ipsa aperiam praesentium temporibus dicta aspernatur architecto! Optio similique autem error deserunt fuga commodi repellat quisquam accusamus laboriosam, voluptatibus quidem sit!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, qui? Dolorem recusandae animi debitis, saepe optio mollitia repudiandae tempore laudantium officia aliquam ipsam minima eaque vitae molestiae ipsum dolor harum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium est laboriosam commodi sed libero eveniet quam obcaecati totam illo? Rerum magnam esse, aliquid autem explicabo aspernatur maiores ullam quod quis.
+                        </p>
+                    </div>
                 </div>
+    
+                <div class="pictures-container">
+                    <div class="text-white text-lg flex sm:hidden">
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim nihil delectus sed asperiores id harum iste. Quidem assumenda, optio iusto commodi dolor, magni deserunt corporis expedita corrupti, molestiae ducimus exercitationem!
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto odio facilis corrupti possimus similique, iste eaque, natus doloribus, eius consequuntur dolor tempore pariatur eveniet neque? Perspiciatis officia doloribus omnis optio.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta saepe in iusto eius vero quae, laudantium fuga eaque ipsum. Quia nostrum accusantium omnis, alias fuga illo ratione porro. Repudiandae, maiores
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere ipsa aperiam praesentium temporibus dicta aspernatur architecto! Optio similique autem error deserunt fuga commodi repellat quisquam accusamus laboriosam, voluptatibus quidem sit!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, qui? Dolorem recusandae animi debitis, saepe optio mollitia repudiandae tempore laudantium officia aliquam ipsam minima eaque vitae molestiae ipsum dolor harum.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium est laboriosam commodi sed libero eveniet quam obcaecati totam illo? Rerum magnam esse, aliquid autem explicabo aspernatur maiores ullam quod quis.
+                        </p>
+                    </div>
+    
+                    <img src="../../../public/images/gif_rendu_final.gif" alt="vidéo de présentation de mon projet">
+                    <img src="../../../public/images/photo-perso.webp" alt="">
+    
+                </div>
+    
             </div>
 
-            <div class="pictures-container">
 
-                <img src="../../../public/images/gif_rendu_final.gif" alt="vidéo de présentation de mon projet">
-                <img src="../../../public/images/photo-perso.webp" alt="">
-
-            </div>
-
-        </div>
         <div class="row">
             <div class="heading-content text-black">
                 <h4 class="bg-white text-center font-unbounded text-[25px]">Logo</h4>
@@ -54,6 +68,8 @@
 
             </div>
         </div>
+
+
 
         <div class="row">
             <div class="heading-content text-black">
@@ -88,6 +104,26 @@
 </template>
 
 <style>
+
+.column{
+    margin: 0 auto;
+}
+
+.row{
+    display: grid;
+    grid-template-rows:auto;
+    grid-gap: 10px;
+    padding: 0px;
+}
+
+.heading-content, h4 {
+    font-size: 35px;
+    padding: 0 15px;
+    position: sticky;
+    top: 0;
+}
+
+
 @media (min-width: 1000px) {
     .parallax-effect {
         background-image: url("../../../public/images/fond_emorphoz.webp");
@@ -104,6 +140,24 @@
         font-size: 36px;
         text-align: center;
       }
+
+      .column{
+    margin: 0 auto;
+    }
+
+    .row{
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-gap: 10px;
+        padding: 10px;
+    }
+
+    .heading-content, h4 {
+        font-size: 35px;
+        padding: 0 15px;
+        position: sticky;
+        top: 0;
+    }
 
     
 }
@@ -173,24 +227,6 @@
 
 .contenair {
     max-width: 6000px;
-}
-
-.column{
-    margin: 0 auto;
-}
-
-.row{
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: 10px;
-    padding: 10px;
-}
-
-.heading-content, h4 {
-    font-size: 35px;
-    padding: 0 15px;
-    position: sticky;
-    top: 0;
 }
 
 </style>
