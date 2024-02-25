20e53373d50c322e137c10d24ba103c07130643b
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2024-02-25 23:03:49 +0100

    photos_popwave

--- src/views/page_projet/PopWaveView.vue
+++ src/views/page_projet/PopWaveView.vue
@@ -189,10 +189,10 @@
         <div class="border-t-4 border-white sm:flex hidden"></div>
 
         <div class="grid xxxs:grid-cols-1 sm:grid-cols-2 gap-0 gap-y-0">
-                <img class="w-full h-full object-contain" src="../../../public/images/paysage_usine.webp" alt="image galerie présentant mon projet sur une animation">
-                <img class="w-full h-full object-contain" src="../../../public/images/fumée_usine.webp" alt="image galerie présentant mon projet sur une animation">
-                <img class="w-full h-full object-contain" src="../../../public/images/telephone_coté.webp" alt="image galerie présentant mon projet sur une animation">
-                <img class="w-full h-full object-contain" src="../../../public/images/usine_200.webp" alt="image galerie présentant mon projet sur une animation">
+                <img class="w-full h-full object-contain" src="../../../public/images/orange_popwave.webp" alt="image galerie présentant mon projet sur une animation">
+                <img class="w-full h-full object-contain" src="../../../public/images/popwave_eclaboussure.png" alt="image galerie présentant mon projet sur une animation">
+                <img class="w-full h-full object-contain" src="../../../public/images/fruity_popwave.png" alt="image galerie présentant mon projet sur une animation">
+                <img class="w-full h-full object-contain" src="../../../public/images/orange_citreon_popwave.png" alt="image galerie présentant mon projet sur une animation">
 
         </div>
     
