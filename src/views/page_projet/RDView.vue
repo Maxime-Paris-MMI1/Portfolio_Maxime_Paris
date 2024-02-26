01524a3e2ca4a521e45808d0be29cb8824e448df
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2024-02-27 00:21:34 +0100

    RD confort et habitat

--- src/views/page_projet/RDView.vue
+++ src/views/page_projet/RDView.vue
@@ -1,3 +1,288 @@
 <template>
-    <h1>rd</h1>
-</template>
+    <div class="derriere16">
+    
+        <header>
+          <Header_total/>
+        </header>
+        
+        
+            <div class="contenair">
+                    <h1 class=" justify-center flex text-[5xl] text-white font-unbounded mt-16 mb-16 carret lg:-mt-[100px]">
+                        <span>
+                            R&D
+                        </span>
+                    </h1>
+            </div>
+    
+            
+
+            <div class="border-t-4 border-white"></div>
+    </div>
+    
+    
+    
+    
+        <div class="column mt-10">
+    
+            <!-- Première partie -->
+    
+    
+                <div class="row">
+                    
+                    <div class="heading-content text-black">
+                        <h4 class="bg-white text-center font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  mb-3">Présentation</h4>
+        
+                        <div class=" mt-10 sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] hidden sm:flex sm:flex-col gap-5">    
+                            <p>  
+                                R&D est un projet professionnel que j'ai réalisé dans le cadre de mon auto-entreprise en tant que freelance.
+                            </p>
+                            <p>
+                                La demande initiale du client était de créer une identité ainsi qu'une charte graphique pour son entreprise de menuiserie.
+                            </p>
+                            <p>
+                                Après plusieurs retours et modifications du logo, voici le résultat final. 
+                            </p>
+                        </div>
+                    </div>
+        
+                    <div class="pictures-container">
+                        <div class=" sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] flex sm:hidden flex-col">   
+                            <p>  
+                                R&D est un projet professionnel que j'ai réalisé dans le cadre de mon auto-entreprise en tant que freelance.
+                            </p>
+                            <p>
+                                La demande initiale du client était de créer une identité ainsi qu'une charte graphique pour son entreprise de menuiserie.
+                            </p>
+                            <p>
+                                Après plusieurs retours et modifications du logo, voici le résultat final. 
+                            </p>
+                        </div>
+    
+                        <div class="flex flex-col gap-10">
+                            <div class="flex flex-col items-center gap-10 mt-10 md:mt-0">
+                                <div class="flex justify-center">
+                                    <img class="xxxs:w-10/12 sm:w-full h-full object-contain max-w-[690px] mb-10" src="../../../public/images/presentation_rd.webp" alt="vidéo de présentation de mon projet">
+                                </div>
+                            </div>
+    
+    
+                        </div>
+        
+        
+                    </div>
+        
+                </div>
+    
+    
+                <!-- Deuxième partie -->
+    
+    
+                <div class="row">
+                    
+                    <div class="heading-content text-black">
+                        <h4 class="bg-white text-center font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  mb-3" id="fonctionnement">Travail effectué</h4>
+        
+                        <div class=" sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] hidden sm:flex sm:flex-col">
+                            <p class="mb-5">
+                                Une version minimaliste du logo a été établie. 
+                            </p>
+                            <p>
+                                J'ai également élaboré une charte graphique complète à ce sujet.
+                            </p>
+                            <p>
+                                C'est un projet enrichissant qui m'a permis de créer une identité pour un client réel.
+                            </p>
+
+                            <img class="xxxs:w-10/12 sm:w-[40%] h-[40%] object-contain max-w-[690px] mt-10" src="../../../public/images/RD-abstrait_sans_slogan_noir.webp" alt="">
+                            
+                        </div>
+                                
+                    </div>
+        
+                    <div class="pictures-container">
+                        <div class="sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] flex sm:hidden flex-col">
+                            <p class="mb-5">
+                                Une version minimaliste du logo a été établie. 
+                            </p>
+                            <p>
+                                J'ai également élaboré une charte graphique complète à ce sujet.
+                            </p>
+                            <p>
+                                C'est un projet enrichissant qui m'a permis de créer une identité pour un client réel.
+                            </p>
+                        </div>
+    
+                        <div class="flex flex-col gap-10">
+                            <div class="flex flex-col items-center gap-10 mt-10 md:mt-0">
+                                <div class="flex justify-center">
+                                    <img class="xxxs:w-10/12 sm:w-[100%] h-full object-contain max-w-[690px] sm:mt-24" src="../../../public/images/charte_graphique.webp" alt="vidéo de présentation de mon projet">
+                                </div>
+
+                                <div class="sm:hidden flex justify-center">
+                                    <img class="xxxs:w-[50%] sm:w-[40%] h-[40%] object-contain max-w-[690px]" src="../../../public/images/RD-abstrait_sans_slogan_noir.webp" alt="">
+
+                                </div>
+                            </div>
+    
+                        </div>
+        
+        
+                    </div>
+        
+                </div>
+
+        </div>
+    
+        <div class="border-t-4 border-white mt-10 sm:flex hidden"></div>
+    
+        <div>
+            <div class="parallax-effect16 flex justify-center items-center">
+                <h5 class="text-center xxxs:w-2/4 sm:w-1/4 xxxs:my-10 sm:my-0 xxxs:bg-white xxxs:text-black sm:text-white sm:bg-black font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  ">Merci</h5>
+            </div>
+        </div>
+
+        <div class="border-t-4 border-white sm:flex hidden"></div>
+    
+        <footer>
+            <Footer_total/>
+        </footer>
+    
+    </template>
+    
+    <style>
+
+@media only screen and (max-width: 1000px) {
+  .vers_bas{
+    opacity: 0;
+    transform: translateY(-150px);
+    animation: deplacement2 2s cubic-bezier(.97,-0.01,.63,1) forwards;
+  }
+
+  @keyframes deplacement2 {
+  0% {
+    opacity: 0;
+    transform: translateY(-150px);
+  }
+
+  70%{
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+
+}
+
+
+
+@media (min-width: 1000px) {
+
+
+.vers_droite {
+  opacity: 0;
+  transform: translateX(-300px);
+  animation: deplacement 2s cubic-bezier(.97,-0.01,.63,1) forwards;
+}
+
+@keyframes deplacement {
+  0% {
+    opacity: 0;
+    transform: translateX(-300px);
+  }
+
+  70% {
+
+    opacity: 0.2;
+    transform: translateX(-120px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+
+
+.parallax-effect16 {
+        background-image: url("../../../public/images/mockup_rd.webp");
+        min-height: 500px;
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+
+      .parallax-effect16 + div {
+        height: 600px;
+        background-image: #000;
+        font-size: 36px;
+        text-align: center;
+      }
+
+
+}
+
+
+
+
+
+.derriere16 {
+  position: relative;
+}
+.derriere16::before {
+  content: "";
+  background-image: url(../../../public/images/image_fond.webp);
+  background-size: cover;
+  opacity: 0.6;
+  position: absolute;
+  background-position: top;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+    
+    </style>
+    
+    <script>
+    
+    import Header_total from '../../components/Header_total.vue';
+    import Footer_total from '../../components/Footer_total.vue';
+    
+    
+    
+    export default {
+      components :{Header_total, Footer_total}
+    
+    }
+    
+    </script>
