5834eb25e1438134d4230cdb2fa60b7bb3ac6084
Maxime-Paris-MMI1 <95071690+Maxime-Paris-MMI1@users.noreply.github.com>
2024-02-26 20:06:15 +0100

    motion gradient

--- src/views/page_projet/MotionGradientView.vue
+++ src/views/page_projet/MotionGradientView.vue
@@ -1,3 +1,304 @@
 <template>
-    <h1>univ</h1>
-</template>
+    <div class="derriere12">
+    
+        <header>
+          <Header_total/>
+        </header>
+        
+        
+            <div class="contenair">
+                    <h1 class=" justify-center flex text-[5xl] text-white font-unbounded mt-16 mb-16 carret lg:-mt-[100px]">
+                        <span>
+                            Motion gradient
+                        </span>
+                    </h1>
+            </div>
+        
+            <div class="flex sm:flex-row xxxs:flex-col items-center justify-center mb-12 xs:mx-10 xxs:mx-5 xxxs:mx-3">
+                <div class="flex flex-col">
+                    <iframe width="560" height="315" class="xxxs:w-[300px] xxxs:h-[168px] xxs:w-[400px] xxs:h-[225px] xfoo:w-[560px] sm:rounded-3xl xxxs:rounded-xl xfoo:h-[315px] sm:w-[800px] sm:h-[450px] z-10" src="https://www.youtube.com/embed/uUiR2EAc6EQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <p class="text-white font-ubuntu xxxs:text-[14px] sm:text-[16px]">Music by <span class="underline"><a href="https://pixabay.com/fr/music/future-basse-abstract-future-bass-162604/" target="_blank">pixabay</a></span></p>
+                </div>
+                <div class="flex sm:flex-col xxxs:flex-row items-center justify-center gap-16 bg-black sm:rounded-r-2xl xxxs:rounded-b-2xl sm:rounded-l-none sm:px-5 sm:py-5 xxxs:px-8 xxxs:py-3 z-0 vers_droite vers_bas">
+
+                    <a href="https://www.instagram.com/p/CxAPuncMYYg/" target="__blank" class="hover:scale-110 ease-in-out duration-300">
+                        <img src="../../../public/images/Instagram_Glyph_White-1.webp" class="sm:w-[51px] xs:w-[40px] xxs:w-[35px] xxxs:w-[30px]" alt="logo Instagram">
+                    </a>
+
+                    <a href="https://www.tiktok.com/@max_portfolio/video/7277194544825830688" target="__blank" class="hover:scale-110 ease-in-out duration-300">
+                        <img src="../../../public/images/tiktok_logo.webp" class="sm:w-[80px] xs:w-[55px] xxs:w-[50px] xxxs:w-[50px]" alt="Logo Behance">
+                    </a>
+
+                </div>
+                
+            </div>
+            
+
+            <div class="border-t-4 border-white"></div>
+    </div>
+    
+    
+    
+    
+        <div class="column mt-10">
+    
+            <!-- Première partie -->
+    
+    
+                <div class="row">
+                    
+                    <div class="heading-content text-black">
+                        <h4 class="bg-white text-center font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  mb-3">Présentation</h4>
+        
+                        <div class=" mt-10 sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] hidden sm:flex sm:flex-col gap-5">    
+                            <p>  
+                                Pour améliorer mes compétences en motion design, 
+                                j'ai décidé de créer une animation sur After Effects utilisant exclusivement des formes géométriques.
+                            </p>
+                            <p>
+                                Ce projet m'a permis de découvrir des nouvelles techniques.
+                            </p>
+                        </div>
+                    </div>
+        
+                    <div class="pictures-container">
+                        <div class=" sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] flex sm:hidden flex-col">   
+                            <p>  
+                                Pour améliorer mes compétences en motion design, 
+                                j'ai décidé de créer une animation sur After Effects utilisant exclusivement des formes géométriques.
+                            </p>
+                            <p>
+                                Ce projet m'a permis de découvrir des nouvelles techniques.
+                            </p>
+                        </div>
+    
+                        <div class="flex flex-col gap-10">
+                            <div class="flex flex-col items-center gap-10 mt-10 md:mt-0">
+                                <div class="flex justify-center">
+                                    <img class="xxxs:w-10/12 sm:w-full h-full object-contain max-w-[690px] mb-10" src="../../../public/images/motion_violet.webp" alt="vidéo de présentation de mon projet">
+                                </div>
+                            </div>
+    
+    
+                        </div>
+        
+        
+                    </div>
+        
+                </div>
+    
+    
+                <!-- Deuxième partie -->
+    
+    
+                <div class="row">
+                    
+                    <div class="heading-content text-black">
+                        <h4 class="bg-white text-center font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  mb-3" id="fonctionnement">Travail effectué</h4>
+        
+                        <div class=" sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] hidden sm:flex sm:flex-col">
+                            <p class="mb-5">
+                                J'ai intégré des dégradés avec un effet de grain pour donner du dynamisme à mon animation. 
+                            </p>
+                            <p>
+                                J'ai également utilisé des techniques de flat design pour rester dans le thème choisi.
+                            </p>
+                            <p>
+                                Les couleurs dans la vidéo sont particulièrement vives et contrastées. 
+                                Ce choix permet de mettre en avant les différentes ambiances, avec des transitions fluides entre les changements de couleurs.
+                            </p>
+
+                            <img class="xxxs:w-10/12 sm:w-full h-full object-contain max-w-[690px] mt-10" src="../../../public/images/contraste_motion.webp" alt="">
+                            
+                        </div>
+                                
+                    </div>
+        
+                    <div class="pictures-container">
+                        <div class="sm:mb-0 sm:mx-10 xxxs:mx-5 xxs:mx-7 xs:mx-9 text-white font-ubuntu xxxs:text-[14px] sm:text-[16px] flex sm:hidden flex-col">
+                            <p class="mb-5">
+                                J'ai intégré des dégradés avec un effet de grain pour donner du dynamisme à mon animation. 
+                            </p>
+                            <p>
+                                J'ai également utilisé des techniques de flat design pour rester dans le thème choisi.
+                            </p>
+                            <p>
+                                Les couleurs dans la vidéo sont particulièrement vives et contrastées. 
+                                Ce choix permet de mettre en avant les différentes ambiances, avec des transitions fluides entre les changements de couleurs.
+                            </p>
+                        </div>
+    
+                        <div class="flex flex-col gap-10">
+                            <div class="flex flex-col items-center gap-10 mt-10 md:mt-0">
+                                <div class="flex justify-center">
+                                    <img class="xxxs:w-10/12 sm:w-[100%] h-full object-contain max-w-[690px] sm:mt-24" src="../../../public/images/carre_assemblage.webp" alt="vidéo de présentation de mon projet">
+                                </div>
+
+                                <div class="sm:hidden flex justify-center">
+                                    <img class="xxxs:w-10/12 sm:w-full h-full object-contain max-w-[690px]" src="../../../public/images/contraste_motion.webp" alt="">
+
+                                </div>
+                            </div>
+    
+                        </div>
+        
+        
+                    </div>
+        
+                </div>
+
+        </div>
+    
+        <div class="border-t-4 border-white mt-10 sm:flex hidden"></div>
+    
+        <div>
+            <div class="parallax-effect10 flex justify-center items-center">
+                <h5 class="text-center xxxs:w-2/4 sm:w-1/4 xxxs:my-10 sm:my-0 xxxs:bg-white xxxs:text-black sm:text-white sm:bg-black font-unbounded font-[500]
+                  lg:text-[35px]
+                  md:text-[35px]
+                  sm:text-[30px]
+                  xs:text-[25px]
+                  xxs:text-[20px]
+                  xxxs:text-[16px]
+                  ">Merci</h5>
+            </div>
+        </div>
+
+        <div class="border-t-4 border-white sm:flex hidden"></div>
+    
+        <footer>
+            <Footer_total/>
+        </footer>
+    
+    </template>
+    
+    <style>
+
+@media only screen and (max-width: 1000px) {
+  .vers_bas{
+    opacity: 0;
+    transform: translateY(-150px);
+    animation: deplacement2 2s cubic-bezier(.97,-0.01,.63,1) forwards;
+  }
+
+  @keyframes deplacement2 {
+  0% {
+    opacity: 0;
+    transform: translateY(-150px);
+  }
+
+  70%{
+    opacity: 0;
+    transform: translateY(-100px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+
+}
+
+
+
+@media (min-width: 1000px) {
+
+
+.vers_droite {
+  opacity: 0;
+  transform: translateX(-300px);
+  animation: deplacement 2s cubic-bezier(.97,-0.01,.63,1) forwards;
+}
+
+@keyframes deplacement {
+  0% {
+    opacity: 0;
+    transform: translateX(-300px);
+  }
+
+  70% {
+
+    opacity: 0.2;
+    transform: translateX(-120px);
+  }
+
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+}
+
+
+
+.parallax-effect10 {
+        background-image: url("../../../public/images/motion_violet.webp");
+        min-height: 500px;
+        background-attachment: fixed;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+      }
+
+      .parallax-effect10 + div {
+        height: 600px;
+        background-image: #000;
+        font-size: 36px;
+        text-align: center;
+      }
+
+
+}
+
+
+
+
+
+.derriere12 {
+  position: relative;
+}
+.derriere12::before {
+  content: "";
+  background-image: url(../../../public/images/motion_orange.webp);
+  background-size: cover;
+  opacity: 0.6;
+  position: absolute;
+  background-position: top;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
+
+    
+    </style>
+    
+    <script>
+    
+    import Header_total from '../../components/Header_total.vue';
+    import Footer_total from '../../components/Footer_total.vue';
+    
+    
+    
+    export default {
+      components :{Header_total, Footer_total}
+    
+    }
+    
+    </script>
